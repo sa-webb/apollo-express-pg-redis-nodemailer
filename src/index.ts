b35a27d8636ff153ad25e36ee6aef6520632ee39
sa-webb <sawebb30@gmail.com>
2020-08-31 17:35:13 -0400

    feat: userResolver

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,18 @@
 import "reflect-metadata"
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
-import mikroOrmConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
+import { __prod__ } from "./constants";
+
+import mikroOrmConfig from "./mikro-orm.config";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
-import { Post } from "./entities/post";
+import { UserResolver } from "./resolvers/user";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
-
-  const posts = await orm.em.find(Post, {})
-  console.log(posts)
 
   const app = express();
   app.get("/", (_, res) => {
@@ -23,7 +21,7 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver, PostResolver],
+      resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
     context: () => ({
