4c5778f0e6a17ff260f9b9165fbe7ec5098f73b9
sa-webb <sawebb30@gmail.com>
2020-09-02 12:49:10 -0400

    feat: redis

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import "reflect-metadata";
-import { MikroORM } from "@mikro-orm/core";
 import express from "express";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { MikroORM } from "@mikro-orm/core";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { __prod__ } from "./constants";
@@ -9,12 +12,33 @@ import mikroOrmConfig from "./mikro-orm.config";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import { MyContext } from "./types";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
 
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
   const app = express();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({ client: redisClient, disableTouch: true }),
+      secret: "ligftrdrtdfkyg",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+        httpOnly: true,
+        secure: __prod__,
+        sameSite: "lax",
+      },
+    })
+  );
+
   app.get("/", (_, res) => {
     res.send("hello");
   });
@@ -24,8 +48,10 @@ const main = async () => {
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({
+    context: ({ req, res }): MyContext => ({
       em: orm.em,
+      req,
+      res,
     }),
   });
 
